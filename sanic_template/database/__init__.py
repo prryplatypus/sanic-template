7f9e9d1d68a1259b2bebdbf517c2c14a25317575
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2022-06-23 23:25:50 +0200

    Remove fixed aiomysql workaround

--- sanic_template/database/__init__.py
+++ sanic_template/database/__init__.py
@@ -18,27 +18,6 @@ def init(app: "App") -> None:
     async def _(*_):
         await pool.connect()
 
-        # Temporary workaround due to aiomysql bug on
-        # timed out connections. See:
-        # https://github.com/aio-libs/aiomysql/pull/660
-        try:
-            import aiomysql
-        except ImportError:
-            return
-
-        pool_ = pool._backend._pool
-        if not isinstance(pool_, aiomysql.Pool):
-            return
-
-        async def _do_ping():
-            while True:
-                async with pool_.acquire() as conn:
-                    await conn.ping()
-                await asyncio.sleep(5)
-
-        app.add_task(_do_ping(), name="aiomysql-ping")
-
     @app.after_server_stop
     async def _(*_):
-        await app.cancel_task("aiomysql-ping", raise_exception=False)
         await pool.disconnect()
