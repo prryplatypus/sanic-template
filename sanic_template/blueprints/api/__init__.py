50d67e5f7f6a32148354062cf6e65127d8cdfff8
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2022-06-24 15:57:39 +0200

    Further cleanup

--- sanic_template/blueprints/api/__init__.py
+++ sanic_template/blueprints/api/__init__.py
@@ -2,17 +2,8 @@ from sanic import Blueprint
 
 from . import v1
 
-# from atlas_api.security import authentication
-
 
 bp = Blueprint.group(
     v1.bp,
     version_prefix="/api/v",
 )
-
-# This has been placed in atlas_api.security.init since
-# we need it to happen prior to parameter injection, which
-# happens during app.signal("http.routing.after"), which is
-# before any middlewares would be called.
-
-# bp.middleware("request")(authentication.middleware)
